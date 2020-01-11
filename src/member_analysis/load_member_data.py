a963518cfaf8a97bb6d1a5f2670f5cb6d6518b3a
unknown <42840614+lustat@users.noreply.github.com>
2020-01-11 18:48:27 +0100

    save output in Excel-file

--- src/member_analysis/load_member_data.py
+++ src/member_analysis/load_member_data.py
@@ -1,16 +1,15 @@
 import datetime
+import os
 import pandas as pd
 from base_utils import rel2fullpath
 from dateutil.relativedelta import relativedelta
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
 
 
-if __name__ == '__main__':
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
+def load_member_data(relative_excel_path: str):
+    input_file = rel2fullpath(relative_excel_path)
 
-    input_file = rel2fullpath('data/members/ExportedPersons_2019.xlsx')
-    print(input_file)
     df = pd.read_excel(input_file, sheet_name='Data')
     original_columns = df.columns
     new_columns = [column.lower().replace('.', '').replace(' ', '_').replace('/', '_').replace(':', '')
@@ -26,14 +25,37 @@ if __name__ == '__main__':
     age_splits = [-1, 7, 12, 16, 20, 25, 100000]
     labels = ['0-7', '8-12', '13-16', '17-20', '21-25', '26+']
     df = df.assign(binned=pd.cut(df['age'], bins=age_splits, labels=labels))
+    df = df.assign(binned=[str(age_bin) for age_bin in df.binned])
+    return df
 
-    print(df)
+
+if __name__ == '__main__':
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+
+    excel_file = 'data/members/ExportedPersons_2019.xlsx'
+    output_excel = rel2fullpath(os.path.join(os.path.split(excel_file)[0], 'PersonAges_2019.xlsx'))
+    df = load_member_data(excel_file)
 
     count_df = df[['kon', 'binned', 'fornamn']].groupby(by=['kon', 'binned']).count()
     count_df = count_df.reset_index()
     count_df.columns = ['Kön', 'Ålder', 'Antal']
 
     print(count_df)
+
+    df_out = df[['fornamn', 'efternamn', 'fodelsedat_personnr', 'kon', 'age', 'binned']]
+    print(df_out)
+    wb = openpyxl.Workbook()
+    worksheet = wb.create_sheet('Data')
+    for r in dataframe_to_rows(df_out, index=False, header=True):
+        worksheet.append(r)
+
+    worksheet = wb.create_sheet('Stat')
+    for r in dataframe_to_rows(count_df, index=False, header=True):
+        worksheet.append(r)
+
+    wb.save(output_excel)
     print('Finished')
 
     # Create output Excel
