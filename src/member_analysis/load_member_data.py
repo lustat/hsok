ca4a2308ff42b89ae3386dc4a2aceb6a824f3aa9
unknown <42840614+lustat@users.noreply.github.com>
2020-01-11 18:21:28 +0100

    change output df column names

--- src/member_analysis/load_member_data.py
+++ src/member_analysis/load_member_data.py
@@ -5,6 +5,10 @@ from dateutil.relativedelta import relativedelta
 
 
 if __name__ == '__main__':
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+
     input_file = rel2fullpath('data/members/ExportedPersons_2019.xlsx')
     print(input_file)
     df = pd.read_excel(input_file, sheet_name='Data')
@@ -23,8 +27,14 @@ if __name__ == '__main__':
     labels = ['0-7', '8-12', '13-16', '17-20', '21-25', '26+']
     df = df.assign(binned=pd.cut(df['age'], bins=age_splits, labels=labels))
 
-    print(df.head(5))
+    print(df)
 
     count_df = df[['kon', 'binned', 'fornamn']].groupby(by=['kon', 'binned']).count()
+    count_df = count_df.reset_index()
+    count_df.columns = ['Kön', 'Ålder', 'Antal']
+
     print(count_df)
     print('Finished')
+
+    # Create output Excel
+
