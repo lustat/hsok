6fc692859db381535a3a671c5034de9e5a3fe609
unknown <42840614+lustat@users.noreply.github.com>
2020-12-28 18:49:26 +0100

    calculate member stats for 2020

--- src/member_analysis/load_member_data.py
+++ src/member_analysis/load_member_data.py
@@ -5,13 +5,13 @@ from base_utils import rel2fullpath
 from dateutil.relativedelta import relativedelta
 import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
+import collections
 
 
-def load_member_data(relative_excel_path: str):
+def load_member_data(relative_excel_path: str, year_to_sheet: collections.defaultdict):
     input_file = rel2fullpath(relative_excel_path)
 
-    df = pd.read_excel(input_file, sheet_name='Data')
-    original_columns = df.columns
+    df = pd.read_excel(input_file, sheet_name=year_to_sheet[year])
     new_columns = [column.lower().replace('.', '').replace(' ', '_').replace('/', '_').replace(':', '')
                   for column in df.columns]
     df.columns = [column.lower().replace('å', 'a').replace('ä', 'a').replace('ö', 'o')
@@ -34,29 +34,33 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
 
-    excel_file = 'data/members/ExportedPersons_2019.xlsx'
-    output_excel = rel2fullpath(os.path.join(os.path.split(excel_file)[0], 'PersonAges_2019.xlsx'))
-    df = load_member_data(excel_file)
+    years = [2019, 2020]
+    year_to_sheet_name = collections.defaultdict(lambda: 'SearchPersons')
+    year_to_sheet_name[2019] = 'Data'
 
-    count_df = df[['kon', 'binned', 'fornamn']].groupby(by=['kon', 'binned']).count()
-    count_df = count_df.reset_index()
-    count_df.columns = ['Kön', 'Ålder', 'Antal']
+    for year in years:
+        excel_file = 'data/members/ExportedPersons_' + str(year) + '.xlsx'
+        df = load_member_data(excel_file, year_to_sheet_name)
 
-    print(count_df)
+        count_df = df[['kon', 'binned', 'fornamn']].groupby(by=['kon', 'binned']).count()
+        count_df = count_df.reset_index()
+        count_df.columns = ['Kön', 'Ålder', 'Antal']
 
-    df_out = df[['fornamn', 'efternamn', 'fodelsedat_personnr', 'kon', 'age', 'binned']]
-    print(df_out)
-    wb = openpyxl.Workbook()
-    worksheet = wb.create_sheet('Data')
-    for r in dataframe_to_rows(df_out, index=False, header=True):
-        worksheet.append(r)
+        print(count_df)
 
-    worksheet = wb.create_sheet('Stat')
-    for r in dataframe_to_rows(count_df, index=False, header=True):
-        worksheet.append(r)
+        df_out = df[['fornamn', 'efternamn', 'fodelsedat_personnr', 'kon', 'age', 'binned']]
+        print(df_out)
 
-    wb.save(output_excel)
+        output_excel = rel2fullpath(os.path.join(os.path.split(excel_file)[0], 'PersonAges_' + str(year) + '.xlsx'))
+        wb = openpyxl.Workbook()
+        worksheet = wb.create_sheet('Data')
+        for r in dataframe_to_rows(df_out, index=False, header=True):
+            worksheet.append(r)
+
+        worksheet = wb.create_sheet('Stat')
+        for r in dataframe_to_rows(count_df, index=False, header=True):
+            worksheet.append(r)
+
+        wb.save(output_excel)
     print('Finished')
-
-    # Create output Excel
 
