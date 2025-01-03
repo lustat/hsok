1b73473899b84faa2ef3cc21eeb7409ff5740946
Klas Bogsjö <klasbogsjo@gmail.com>
2025-01-03 10:58:39 +0100

    statistics 2024

--- src/member_analysis/load_member_data.py
+++ src/member_analysis/load_member_data.py
@@ -56,11 +56,11 @@ if __name__ == '__main__':
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
 
-    output_directory = SOURCE_DIR + '\member_analysis'
+    output_directory = f'{SOURCE_DIR}/member_analysis'
     if not os.path.exists(output_directory):
         raise ValueError('Unknown folder ' + output_directory)
 
-    years = [2019, 2020, 2021, 2022, 2023]
+    years = [2019, 2020, 2021, 2022, 2023, 2024]
     year_to_sheet_name = collections.defaultdict(lambda: 'SearchPersons')
     year_to_sheet_name[2019] = 'Data'
     excel_flag = False
@@ -116,13 +116,13 @@ if __name__ == '__main__':
     this_year = this_year.assign(bin_number=this_year['Ålder'].replace(cat_to_num))
     this_year = this_year.sort_values(by='bin_number')
 
-    this_year = this_year.reset_index()
+    this_year = this_year.reset_index(drop=True)
     this_year.to_excel(output_directory + '/this_year.xlsx', index=False)
 
     g = sns.catplot(
         data=this_year, kind="bar",
         x="Ålder", y="Antal", hue="Kön",
-        palette="dark", alpha=.6, height=6
+        palette=["Red", "Blue"], alpha=.6, height=6,
     )
     g.despine(left=True)
     g.legend.set_title("")
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     ax = g.facet_axis(0, 0)
     add_labels(ax)
     add_frame_to_axis(ax)
-    g.fig.set_size_inches(7, 4)
+    g.fig.set_size_inches(7, 3)
 
     g.savefig('this_year.png', dpi=300)
 
@@ -141,9 +141,23 @@ if __name__ == '__main__':
     women = this_year.loc[this_year["Kön"] == 'Kvinna'].Antal.sum()
     total = this_year["Antal"].sum()
 
-    print(' ')
-    print('Medlemsstatistik: ')
-    print(' ')
-    print(f'Antalet klubbmedlemmar var {total} vid verksamhetsårets slut. Antalet män {men} st och antalet kvinnor {women} st.')
-    print('Finished')
+    text_result = [str(f"Antalet klubbmedlemmar var {total} vid verksamhetsårets slut. "
+                       f"Antalet män {men} st och antalet kvinnor {women} st."), ""]
+    for gender in this_year["Kön"].unique():
+        gender_data = this_year.loc[this_year["Kön"] == gender]
+        if gender == 'Man':
+            gender_string = 'Män'
+        else:
+            gender_string = 'Kvinnor'
+        gender_result = []
+        for key, row in gender_data.iterrows():
+            gender_result.append(f"{gender_string} {row['Ålder']} år: {row['Antal']} st.")
+        gender_result = " ".join(gender_result)
+        text_result.append(gender_result)
 
+    text_result.append(" ")
+    text_result.append(f"Styrelsen tackar alla som gjort en insats för klubben under året och hoppas på ett "
+                       f"riktigt bra verksamhetsår {str(this_year['År'].unique()[0] + 1)}.")
+    print("\n".join(text_result))
+    print("")
+#
